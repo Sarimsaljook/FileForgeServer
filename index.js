0907d3a0fab3380f21d8ce5f9cc738c1356c3d05
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2024-05-25 23:45:25 +0500

    Add Read File Endpoint (Note: Optimize Speed Later)

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS
 app.use(cors());
 
+const parseContentType = (responseData) => {
+
+   // Convert responseData to string if it's not already a string
+   responseData = responseData.toString();
+
+    // Use a regular expression to find the content type
+    const match = responseData.match(/Content-Type: (.*)/i);
+    
+    // If match is found, return the content type, otherwise return a default value
+    return match ? match[1].trim() : 'application/octet-stream';
+};
+
 // Define a route to call the ngrok URL
 app.get('/ngrok-reference-user-directory', async (req, res) => {
   try {
@@ -41,6 +53,26 @@ app.post('/ngrok-reference-upload-file', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch data from ngrok URL' });
   }
 });
+
+app.get('/ngrok-reference-read-file', async (req, res) => {
+  try { 
+    const response = await axios.get('https://963d-119-155-18-236.ngrok-free.app/read', {
+        headers: {
+            uid: req.headers.uid,
+            'file-name' : req.headers['file-name'],
+            'file-path' : req.headers['file-path'] 
+        },
+        responseType: 'arraybuffer'
+    });
+       res.setHeader('Content-Type', parseContentType(response.data));
+       res.send(response.data);
+  } catch (error) {
+    console.error('Error calling ngrok URL:', error);
+    res.status(500).json({ error: 'Failed to fetch data from ngrok URL' });
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
